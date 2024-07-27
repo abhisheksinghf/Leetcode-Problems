5965ed515a43f58b335281c5325c7e1460930572
abhisheksinghf <abhisheksinghfulanekar@gmail.com>
2024-07-27 16:07:45 +0530

    count occurances

--- Binary-Search/Binary Count Occurance/count_occurance_bs.c
+++ Binary-Search/Binary Count Occurance/count_occurance_bs.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int bs_first_occurance(int *arr, int size, int key)
+int bs_occurance(int *arr, int size, int key)
 {
-    int low, mid, high, result = -1;
-    low = 0;
-    high = size - 1;
+    int low = 0, mid, high = size - 1;
+    int first = -1, last = -1;
+
 
     while (low <= high)
     {
@@ -12,7 +12,7 @@ int bs_first_occurance(int *arr, int size, int key)
 
         if (key == arr[mid])
         {
-            result = mid;
+            first = mid;
             high = mid - 1;
         }
         else if (key < arr[mid])
@@ -24,13 +24,14 @@ int bs_first_occurance(int *arr, int size, int key)
             low = mid + 1;
         }
     }
-    return result;
-}
 
-int bs_last_occurance(int *arr, int size, int key)
-{
-    int low, mid, high, result = -1;
-    low = 0;
+    if (first == -1) 
+    {
+        return 0;
+    }
+
+    // Find last occurrence
+    low = first; 
     high = size - 1;
 
     while (low <= high)
@@ -39,7 +40,7 @@ int bs_last_occurance(int *arr, int size, int key)
 
         if (key == arr[mid])
         {
-            result = mid;
+            last = mid;
             low = mid + 1;
         }
         else if (key < arr[mid])
@@ -51,27 +52,27 @@ int bs_last_occurance(int *arr, int size, int key)
             low = mid + 1;
         }
     }
-    return result;
+
+    return last - first + 1;
 }
 
 int main()
 {
     int arr[] = {1, 3, 3, 3, 3, 3, 3, 3, 8, 9, 9, 10}; // sorted array
-
     int size = sizeof(arr) / sizeof(arr[0]);
-    int key = 10;
-    int first = 0;
-    int last = 0;
+    int key = 9;
+    int count;
 
-    first = bs_first_occurance(arr, size, key);
-    last = bs_last_occurance(arr, size, key);
+    count = bs_occurance(arr, size, key);
 
-    if (first != -1 && last != -1)
+    if (count > 0)
     {
-        printf("Element first and last occured at index %d amd %d", first,last);
+        printf("Count of %d is %d\n", key, count);
     }
     else
     {
-        printf("Element not found!!");
+        printf("Element not found!!\n");
     }
+
+    return 0;
 }
