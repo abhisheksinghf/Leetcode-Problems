6028a8666c395f2bb431866582d4bb193d1e15a4
abhisheksinghf <abhisheksinghfulanekar@gmail.com>
2024-07-29 20:05:57 +0530

    position in infinite sorted array

---
+++ Binary-Search/Binary Position of Element in Infinite Sorted array/bs_infinite.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int binary_search(int *arr, int size, int key)
+{
+    int low, high, mid;
+    low = 0;
+    high = 1;
+
+    //extra logic
+    while(key > arr[high])
+    {
+        low = high;
+        high = high * 2;
+    }
+    // printf("%d", high);
+    // printf("%d", low);
+    while (key != arr[mid] && low <= high)
+    {
+        mid = low + (high - low) / 2;
+
+        if (key == arr[mid])
+        {
+            return mid;
+        }
+        else if (key < arr[mid])
+        {
+            high = mid - 1;
+        }
+
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // sorted array
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 9;
+    int index = 0;
+
+    index = binary_search(arr, size, key);
+
+    if (index)
+    {
+        printf("Element found at index %d", index);
+    }
+    else
+    {
+        printf("Element not found");
+    }
+}
