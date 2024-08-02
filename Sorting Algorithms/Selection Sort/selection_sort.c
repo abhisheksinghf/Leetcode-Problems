b7c135a89e61d3a9bcae93db02a3e4ebfc565518
abhisheksinghf <abhisheksinghfulanekar@gmail.com>
2024-08-02 11:32:18 +0530

    selection sort

---
+++ Sorting Algorithms/Selection Sort/selection_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void display(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+}
+void selectionSort(int *arr, int size)
+{
+    int i, j;
+    for (i = 0; i < size - 1; i++)
+    {
+        int min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if(min != i)
+        {
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+        }
+    }
+
+    display(arr, size);
+}
+
+int main()
+{
+    int arr[] = {6, 3, 8, 2, 1, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, size);
+}
